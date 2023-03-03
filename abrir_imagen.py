20948fb929a4c6bdba6a3231c6718219a11f8a30
LABORATORIOS IES <demo@iesac.edu.mx>
2023-03-02 19:25:07 -0600

    Validaciones del menu

--- abrir_imagen.py
+++ abrir_imagen.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import sys
 import cv2
 import filtros
 import numpy as np
@@ -6,45 +7,91 @@ import numpy as np
 
 
 def main():
-    sg.theme('LightGreen')
+    sg.theme('DarkGray15')
+
+    menu_def = [['Archivo', ['Abrir', 'Guardar', 'Salir']],
+                ['Filtros', ['Imagen Original', 'Negativo', 'Logaritmica', 'Cosenoidal', 'Borde Gradiente','Vaciar Imagen'], ],
+                ['Ayuda', 'Acerca de'], ]
 
     layout = [
-        [sg.Text('Filtros', size=(60, 1), justification='center')],
-        [sg.Image(filename='', key='-IMAGE-')],
-        [sg.Button('Abrir', size=(10,1)),sg.Button('Negativo', size=(10,1)),sg.Button('Logaritmica', size=(10,1)),sg.Button('Cosenoidal', size=(10,1)), sg.Button('Guardar', size=(10,1)), sg.Button('Exit', size=(10, 1))]
+        [sg.Text('Editor de Imagenes con Python', size=(80, 1), justification='center')],
+        [sg.Menu(menu_def, )],
+        [sg.Image(filename='', key='-IMAGE-')]
     ]
-    imagen = np.ones((700,700))
-    window = sg.Window('OpenCV Integration', layout, location=(800, 400))
+
+    imagen = np.ones((550,700))
+    window = sg.Window('Mi Primer editor de imagenes en Python', layout, location=(800, 400))
 
     while True:
         event, values = window.read(timeout=20)
 
-        if event == 'Exit' or event == sg.WIN_CLOSED:
+        if event == 'Salir' or event == sg.WIN_CLOSED:
+            sg.popup('Gracias por usar el programa')
             break
 
+        if event == 'Imagen Original':
+            try:
+                imagen = cv2.imread(filename)
+                sg.popup('Mostrando la imagen Original')
+            except Exception:
+                sg.popup('Por favor, elija una imagen')
+                pass
+
         if event == 'Negativo':
-            print("Aplicar negativo")
-            imagen = filtros.negativo(imagen)
+            try:
+                imagen = filtros.negativo(imagen)
+                sg.popup('Mostrando la imagen Negativa')
+            except Exception:
+                sg.popup('Por favor, elija una imagen')
+                pass
 
         if event == 'Logaritmica':
-            print("Aplicar logaritmica")
-            imagen = filtros.logaritmica(imagen)
+            try:
+                imagen = filtros.logaritmica(imagen)
+                sg.popup('Mostrando la imagen con filtro logaritmico')
+            except Exception:
+                sg.popup('Por favor, elija una imagen')
+                pass
 
         if event == 'Cosenoidal':
-            print("Aplicar cosenoidal")
-            imagen = filtros.cosenoidal(imagen)
+            try:
+                imagen = filtros.cosenoidal(imagen)
+                sg.popup('Mostrando la imagen con filtro cosenoidal')
+            except Exception:
+                sg.popup('Por favor, elija una imagen')
+                pass
+
+        if event == 'Borde Gradiente':
+            try:
+                imagen = filtros.borde_gradiente(imagen)
+                sg.popup('Mostrando la imagen con borde gradiente')
+            except Exception:
+                sg.popup('Por favor, elija una imagen')
+                pass
+
+        if event == 'Acerca de':
+            sg.popup('Aplicacion de Filtro de Imagenes con Python', 'Version 1.1', 'Cornbreadse7')
+
+        if event == 'Vaciar Imagen':
+            imagen = np.ones((500, 500))
 
         if event == 'Abrir':
-            print("Abrir imagen")
-            filename = sg.popup_get_file('Abrir archivo (PNG, JPG)') # Abrir solo imagenes JPG o PNG
-            imagen = cv2.imread(filename) #Validar que el nombre de archivo no este en blanco
-            #imagen = cv2.imread("images/004.png",0)
-        
+            filename = sys.argv[1] if len(sys.argv) > 1 else sg.popup_get_file('Abrir archivo (PNG, JPG)')
+
+            if not filename:
+                sg.popup("Por favor, escoja una imagen")
+            else:
+                imagen = cv2.imread(filename)  # Validar que el nombre de archivo no este en blanco
+                # imagen = cv2.imread("images/004.png",0)
+
         if event == 'Guardar':
-            filename = sg.popup_get_file('Guardar Fotografia (PNG) to save to', save_as=True)
-            #ruta = sg.popup_get_folder(title='Guardar Fotografia', message="Carpeta destino")
-            cv2.imwrite(filename, imagen)
-        
+            filename = sys.argv[1] if len(sys.argv) > 1 else sg.popup_get_file('Guardar Fotografia (PNG) to save to', save_as=True)
+            if not filename:
+                sg.popup("Por favor, escoja una ruta")
+            else:
+                # ruta = sg.popup_get_folder(title='Guardar Fotografia', message="Carpeta destino")
+                cv2.imwrite(filename, imagen)
+
         imgbytes = cv2.imencode('.png', imagen)[1].tobytes()
         window['-IMAGE-'].update(data=imgbytes)
 
---
+++ abrir_imagen_1_0.py
@@ -0,0 +1,54 @@
+import PySimpleGUI as sg
+import cv2
+import filtros
+import numpy as np
+##from datetime import date
+
+
+def main():
+    sg.theme('LightGreen')
+
+    layout = [
+        [sg.Text('Filtros', size=(60, 1), justification='center')],
+        [sg.Image(filename='', key='-IMAGE-')],
+        [sg.Button('Abrir', size=(10,1)),sg.Button('Negativo', size=(10,1)),sg.Button('Logaritmica', size=(10,1)),sg.Button('Cosenoidal', size=(10,1)), sg.Button('Guardar', size=(10,1)), sg.Button('Exit', size=(10, 1))]
+    ]
+    imagen = np.ones((500,500))
+    window = sg.Window('OpenCV Integration', layout, location=(800, 400))
+
+    while True:
+        event, values = window.read(timeout=20)
+
+        if event == 'Exit' or event == sg.WIN_CLOSED:
+            break
+
+        if event == 'Negativo':
+            print("Aplicar negativo")
+            imagen = filtros.negativo(imagen)
+
+        if event == 'Logaritmica':
+            print("Aplicar logaritmica")
+            imagen = filtros.logaritmica(imagen)
+
+        if event == 'Cosenoidal':
+            print("Aplicar cosenoidal")
+            imagen = filtros.cosenoidal(imagen)
+
+        if event == 'Abrir':
+            print("Abrir imagen")
+            filename = sg.popup_get_file('Abrir archivo (PNG, JPG)') # Abrir solo imagenes JPG o PNG
+            imagen = cv2.imread(filename) #Validar que el nombre de archivo no este en blanco
+            #imagen = cv2.imread("images/004.png",0)
+        
+        if event == 'Guardar':
+            filename = sg.popup_get_file('Guardar Fotografia (PNG) to save to', save_as=True)
+            #ruta = sg.popup_get_folder(title='Guardar Fotografia', message="Carpeta destino")
+            cv2.imwrite(filename, imagen)
+        
+        imgbytes = cv2.imencode('.png', imagen)[1].tobytes()
+        window['-IMAGE-'].update(data=imgbytes)
+
+    window.close()
+
+
+main()
